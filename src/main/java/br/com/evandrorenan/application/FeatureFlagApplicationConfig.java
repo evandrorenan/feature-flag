a22329d7b80f29edfe4bd76882785238a24a0ead
renan3006@hotmail.com <renan3006@hotmail.com>
2025-01-26 22:36:08 -0300

    List environment variables

--- src/main/java/br/com/evandrorenan/application/FeatureFlagApplicationConfig.java
+++ src/main/java/br/com/evandrorenan/application/FeatureFlagApplicationConfig.java
@@ -2,13 +2,23 @@ package br.com.evandrorenan.application;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 import java.util.Map;
 
 @Configuration
+@Slf4j
 public class FeatureFlagApplicationConfig {
+
+    private final ConfigurableEnvironment environment;
+
+    public FeatureFlagApplicationConfig(ConfigurableEnvironment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     public ObjectMapper buildObjectMapper() {
@@ -18,5 +28,15 @@ public class FeatureFlagApplicationConfig {
     @Bean
     public TypeReference<Map<String, Object>> buildTypeReference() {
         return new TypeReference<Map<String, Object>>() {};
+    }
+
+    @Bean
+    public CommandLineRunner printEnvironmentVariables() {
+        return args -> {
+            log.info("Listing all environment variables");
+            this.environment.getSystemEnvironment().forEach((k, v) -> {
+                log.info("  {}:{}", k, v);
+            });
+        };
     }
 }
