fb2dc8c01fd35ad421b84ff48d2bb9e69d2de3ec
renan3006@hotmail.com <renan3006@hotmail.com>
2025-05-11 23:39:56 -0300

    Feat: Add database-less profile and improve application configuration
    This commit introduces a new `disabled-database` profile to allow running the application without a database connection. It also improves the application configuration and related components.
    
    Key changes:
    
    -   Add `DisabledDatabaseConfig` to exclude `DataSourceAutoConfiguration` when the `disabled-database` profile is active.
    -   Add `ResourceFile` bean to `DisabledDatabaseConfig` for resource file handling.
    -   Refactor `FeatureFlagApplicationConfig`:
        -   Remove constructor and inject dependencies.
        -   Add `RestTemplate` bean.
        -   Add `RequestDetailsLogger` bean.
    -   Rename `flagType` to `type` in `feature-flags.txt` for consistency.
    
    These changes provide flexibility in deployment scenarios and enhance the application's configurability.

--- src/main/java/br/com/evandrorenan/application/DisabledDatabaseConfig.java
+++ src/main/java/br/com/evandrorenan/application/DisabledDatabaseConfig.java
@@ -2,11 +2,19 @@ package br.com.evandrorenan.application;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import static br.com.evandrorenan.infra.adapters.openfeature.TextFileFeatureFlagAdapter.*;
 
 @Configuration
 @Profile("disabled-database")
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 public class DisabledDatabaseConfig {
+
+    @Bean
+    public ResourceFile buildResourceFileName() {
+        return new ResourceFile("feature-flags.txt");
+    }
 }
--- src/main/java/br/com/evandrorenan/application/FeatureFlagApplicationConfig.java
+++ src/main/java/br/com/evandrorenan/application/FeatureFlagApplicationConfig.java
@@ -1,25 +1,24 @@
 package br.com.evandrorenan.application;
 
+import br.com.evandrorenan.domain.ports.out.RequestDetailsLogger;
+import br.com.evandrorenan.infra.adapters.log.DefaultRequestDetailsLogger;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 @Configuration
 @Slf4j
+@NoArgsConstructor
 public class FeatureFlagApplicationConfig {
-
-    private final ConfigurableEnvironment environment;
-
-    public FeatureFlagApplicationConfig(ConfigurableEnvironment environment) {
-        this.environment = environment;
-    }
 
     @Bean
     public ObjectMapper buildObjectMapper() {
@@ -27,16 +26,17 @@ public class FeatureFlagApplicationConfig {
     }
 
     @Bean
-    public TypeReference<Map<String, Object>> buildTypeReference() {
-        return new TypeReference<Map<String, Object>>() {};
+    public RestTemplate buildRestTemplateObject() {
+        return new RestTemplate();
     }
 
     @Bean
-    @Order(1)
-    public CommandLineRunner printEnvironmentVariables() {
-        return args -> {
-            log.info("Listing all environment variables");
-            this.environment.getSystemEnvironment().forEach((k, v) -> log.info("  {}:{}", k, v));
-        };
+    public RequestDetailsLogger buildRequestDetailsLogger() {
+        return new DefaultRequestDetailsLogger();
+    }
+
+    @Bean
+    public TypeReference<Map<String, Object>> buildTypeReference() {
+        return new TypeReference<Map<String, Object>>() {};
     }
 }
